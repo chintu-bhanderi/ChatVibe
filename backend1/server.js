d9f803f3b193531e0cbd098b584bb394a4f8d1f1
Chintu <raxitbhanderi@gmail.com>
2023-01-08 09:12:23 +0530

    move globle packege.json

--- backend1/server.js
+++ backend1/server.js
@@ -4,7 +4,7 @@ const app = express();   // express appication..
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-    res.send('This is backend server');
+    res.send('This is backend server1');
 })
 
 app.listen(PORT,()=>{
