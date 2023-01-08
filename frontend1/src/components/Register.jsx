6c38656fd4fc75dc21aafd054cacaa46f48ca218
Chintu <raxitbhanderi@gmail.com>
2023-01-08 10:21:15 +0530

    1.4 Create Router Design Login and Register Page Part 4
    Design for Login-page

--- frontend1/src/components/Register.jsx
+++ frontend1/src/components/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
     <div className="register">
         <div className="card">
             <div className="card-header">
-                <h3>This is Register</h3>
+                <h3>Register</h3>
 
             </div>
             <div className="card-body">
