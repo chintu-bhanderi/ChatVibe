8a61ed80c163f756399059fdef46b6c3e70a0590
Chintu <raxitbhanderi@gmail.com>
2023-01-08 10:43:40 +0530

    1.5 redux setup
    Redux setup in store/index.js file

---
+++ frontend1/src/store/index.js
@@ -0,0 +1,14 @@
+import {legacy_createStore as createStore,compose,combineReducers,applyMiddleware} from 'redux';
+
+import thunkMiddleware from 'redux-thunk';
+
+const rootReducer = combineReducers({
+
+})
+
+const middleware = [thunkMiddleware];
+
+const store = createStore(rootReducer,compose(applyMiddleware(...middleware),
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() ));
+
+export default store;
