d6b59390879a73580f8c69827fa3caf2fd44d782
Chintu <raxitbhanderi@gmail.com>
2023-01-19 18:26:18 +0530

    Reducer SuccessMessage
    Decode Token
    Return Success Msg

--- frontend1/src/store/actions/authAction.js
+++ frontend1/src/store/actions/authAction.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {REGISTER_FAIL} from "../types/authType";
+import {REGISTER_FAIL,REGISTER_SUCCESS} from "../types/authType";
 
 export const userRegister = (data) => {
      return async (dispatch) => {
@@ -13,7 +13,16 @@ export const userRegister = (data) => {
           try{
                console.log("data-> ",data);
                const response = await axios.post('/api/messenger/user-register',data);
-               console.log(response.data);
+               // console.log(response.data);
+               localStorage.setItem('authToken',response.data.token);
+
+               dispatch({
+                    type : REGISTER_SUCCESS,
+                    payload:{
+                         successMessage: response.data.successMessage,
+                         token : response.data.token
+                    }
+               })
 
           } catch(error){
                // console.log('errorAuth-> ',error)
--- frontend1/src/store/reducers/authReducer.js
+++ frontend1/src/store/reducers/authReducer.js
@@ -1,4 +1,5 @@
-import { REGISTER_FAIL } from "../types/authType";
+import { REGISTER_FAIL,REGISTER_SUCCESS  } from "../types/authType";
+import deCodeToken from 'jwt-decode';
 
 const authState = {
     loading : true,
@@ -8,6 +9,14 @@ const authState = {
     myInfo : ''        // Info about which user is login ..
 }
 
+const tokenDecode = (token) =>{
+    const tokenDecoded = deCodeToken(token);
+    const expTime = new Date(tokenDecoded.exp*1000);
+    if(new Date() > expTime){
+         return null;
+    }
+    return tokenDecoded;
+}
 
 export const authReducer = (state = authState, action) => {
     const {payload,type} = action;
@@ -20,6 +29,18 @@ export const authReducer = (state = authState, action) => {
               myInfo : '',
               loading : true
          }
+    }
+
+    if(type === REGISTER_SUCCESS){
+        const myInfo = tokenDecode(payload.token);
+        return{
+            ...state,
+            myInfo : myInfo,
+            successMessage : payload.successMessage,
+            error : '',
+            authenticate : true,
+            loading: false  
+       }
     }
 
     return state;
