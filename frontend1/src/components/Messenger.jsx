c63e076df8a3206a417b59f4f7ea8f06d8d91ac6
Chintu <raxitbhanderi@gmail.com>
2023-01-26 11:00:40 +0530

    Handle Current Friend - Input field

--- frontend1/src/components/Messenger.jsx
+++ frontend1/src/components/Messenger.jsx
@@ -1,76 +1,105 @@
-import React, { useEffect } from 'react';
-import { FaEllipsisH,FaEdit,FaSistrix } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaEllipsisH, FaEdit, FaSistrix } from "react-icons/fa";
 import ActiveFriend from './ActiveFriend';
 import Friends from './Friends';
 import RightSide from './RightSide';
-import {useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getFriends } from '../store/actions/messengerAction';
 
 const Messenger = () => {
 
-    const {friends} = useSelector(state=>state.messenger);
-    // if user is login then and then should showing messenger page..
-    const {myInfo} = useSelector(state => state.auth);
+     const [currentfriend, setCurrentFriend] = useState('');
+     const [newMessage, setNewMessage] = useState('');
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(getFriends());
-    },[]);
+     const inputHendle = (e) => {
+          setNewMessage(e.target.value);
+     
+     }
 
-    return (
-         <div className='messenger'>
-  <div className='row'>
-       <div className='col-3'>
-            <div className='left-side'>
-                 <div className='top'>
-                      <div className='image-name'>
-                           <div className='image'>
-                           <img src={`./image/${myInfo.image}`} alt='' />
-  
-                           </div>
-                           <div className='name'>
-                           <h3>{myInfo.userName} </h3>
-                           </div>
-                         </div>
-  
-                         <div className='icons'>
-                              <div className='icon'>
-                                <FaEllipsisH />
+     const sendMessage = (e) => {
+          e.preventDefault();
+          console.log(newMessage);
+     
+      }
+
+     console.log(currentfriend);
+
+     const { friends } = useSelector(state => state.messenger);
+     // if user is login then and then should showing messenger page..
+     const { myInfo } = useSelector(state => state.auth);
+
+     const dispatch = useDispatch();
+     useEffect(() => {
+          dispatch(getFriends());
+     }, []);
+
+     useEffect(() => {
+          if(friends && friends.length > 0)
+          setCurrentFriend(friends[0])
+ 
+      },[friends]);
+
+     return (
+          <div className='messenger'>
+               <div className='row'>
+                    <div className='col-3'>
+                         <div className='left-side'>
+                              <div className='top'>
+                                   <div className='image-name'>
+                                        <div className='image'>
+                                             <img src={`./image/${myInfo.image}`} alt='' />
+
+                                        </div>
+                                        <div className='name'>
+                                             <h3>{myInfo.userName} </h3>
+                                        </div>
+                                   </div>
+
+                                   <div className='icons'>
+                                        <div className='icon'>
+                                             <FaEllipsisH />
+                                        </div>
+                                        <div className='icon'>
+                                             <FaEdit />
+                                        </div>
+                                   </div>
                               </div>
-                              <div className='icon'>
-                                    <FaEdit/> 
-                              </div>
-                         </div>
-                 </div>
-  
-                 <div className='friend-search'>
-                      <div className='search'>
-                      <button> <FaSistrix /> </button>
-                      <input type="text" placeholder='Search' className='form-control' />
-                      </div>
-                 </div>
-                 <div className='active-friends'>
-                    <ActiveFriend />
-                </div>
 
-                <div className='friends'>
-                {
-                    friends && friends.length>0 ? friends.map((fd) => <div className='hover-friend'> 
-                        <Friends friend={fd} />
-                        </div> ) : 'No Friend'
-                    } 
+                              <div className='friend-search'>
+                                   <div className='search'>
+                                        <button> <FaSistrix /> </button>
+                                        <input type="text" placeholder='Search' className='form-control' />
+                                   </div>
+                              </div>
+                              <div className='active-friends'>
+                                   <ActiveFriend />
+                              </div>
+
+                              <div className='friends'>
+                                   {
+                                        friends && friends.length > 0 ? friends.map((fd) => <div onClick={() => setCurrentFriend(fd)} className='hover-friend'>
+                                             <Friends friend={fd} />
+                                        </div>) : 'No Friend'
+                                   }
+                              </div>
+
+                         </div>
+
+                    </div>
+
+                    {
+                         currentfriend ? <RightSide
+                              currentfriend={currentfriend}
+                              inputHendle={inputHendle}
+                              newMessage={newMessage}
+                              sendMessage={sendMessage}
+                         /> : 'Please Select your Friend'
+                    }
+
                </div>
 
-            </div>
-  
-                   </div>
+          </div>
+     )
+};
 
-                    <RightSide />
-
-              </div>
-  
-         </div>
-    )
-  };
-  
-  export default Messenger;
+export default Messenger;
--- frontend1/src/components/RightSide.jsx
+++ frontend1/src/components/RightSide.jsx
@@ -1,57 +1,64 @@
 import React from 'react';
-import { FaPhoneAlt,FaVideo,FaRocketchat } from "react-icons/fa";
+import { FaPhoneAlt, FaVideo, FaRocketchat } from "react-icons/fa";
 import Message from './Message';
 import MessageSend from './MessageSend';
 import FriendInfo from './FriendInfo';
 
-const RightSide = () => {
-  return (
-       <div className='col-9'>
-            <div className='right-side'>
-               <input type="checkbox" id='dot' />
-                 <div className='row'>
-                      <div className='col-8'>
-          <div className='message-send-show'>
-               <div className='header'>
-                    <div className='image-name'>
-                         <div className='image'>
-                         <img src='/image/20003ariyan.jpg' alt='' />
+const RightSide = (props) => {
 
+     const {currentfriend,inputHendle,newMessage,sendMessage} = props;
+
+     return (
+          <div className='col-9'>
+               <div className='right-side'>
+                    <input type="checkbox" id='dot' />
+                    <div className='row'>
+                         <div className='col-8'>
+                              <div className='message-send-show'>
+                                   <div className='header'>
+                                        <div className='image-name'>
+                                             <div className='image'>
+                                                  <img src={`./image/${currentfriend.image}`} alt='' />
+
+                                             </div>
+                                             <div className='name'>
+                                                  <h3> {currentfriend.userName} </h3>
+                                             </div>
+                                        </div>
+
+                                        <div className='icons'>
+                                             <div className='icon'>
+                                                  <FaPhoneAlt />
+                                             </div>
+
+                                             <div className='icon'>
+                                                  <FaVideo />
+                                             </div>
+
+                                             <div className='icon'>
+                                                  <label htmlFor='dot'> <FaRocketchat /> </label>
+                                             </div>
+
+                                        </div>
+                                   </div>
+                                   <Message />
+                                   <MessageSend
+                                   inputHendle = {inputHendle}
+                                   newMessage = {newMessage}
+                                   sendMessage = {sendMessage}
+                                   />
+                              </div>
                          </div>
-                         <div className='name'>
-                              <h3> Raxit </h3>
+
+                         <div className='col-4'>
+                              <FriendInfo currentfriend={currentfriend} />
                          </div>
+
+
                     </div>
-
-          <div className='icons'>
-     <div className='icon'>
-          <FaPhoneAlt/>
-     </div>
-
-     <div className='icon'>
-          <FaVideo/>
-     </div>
-
-     <div className='icon'>
-          <label htmlFor='dot'> <FaRocketchat/> </label>  
-     </div>
-
-    </div>
-         </div>
-          <Message />
-          <MessageSend />
-             </div>
-                      </div>  
-
-                 <div className='col-4'>
-                    <FriendInfo />
-               </div>  
-
-
-                 </div>
-            </div>
-       </div>
-  )
+               </div>
+          </div>
+     )
 };
 
 export default RightSide;
