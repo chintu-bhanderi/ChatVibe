8a8aec25f94db3bccd430a2e0aa0d38c1f4bd1d2
Chintu <raxitbhanderi@gmail.com>
2023-01-26 16:12:19 +0530

    Message in chat is Dynamic
    message is comes from db.
    and display acording user and current select user.

--- frontend1/src/components/Messenger.jsx
+++ frontend1/src/components/Messenger.jsx
@@ -4,7 +4,7 @@ import ActiveFriend from './ActiveFriend';
 import Friends from './Friends';
 import RightSide from './RightSide';
 import { useDispatch, useSelector } from 'react-redux';
-import { getFriends,messageSend} from '../store/actions/messengerAction';
+import { getFriends,messageSend,getMessage } from '../store/actions/messengerAction';
 
 const Messenger = () => {
 
@@ -28,7 +28,7 @@ const Messenger = () => {
 
      console.log(currentfriend);
 
-     const { friends } = useSelector(state => state.messenger);
+     const { friends,message } = useSelector(state => state.messenger);
      // if user is login then and then should showing messenger page..
      const { myInfo } = useSelector(state => state.auth);
 
@@ -42,6 +42,10 @@ const Messenger = () => {
           setCurrentFriend(friends[0])
  
       },[friends]);
+
+     useEffect(() => {
+     dispatch(getMessage(currentfriend._id))
+     },[ currentfriend?._id]);
 
      return (
           <div className='messenger'>
@@ -97,6 +101,7 @@ const Messenger = () => {
                               inputHendle={inputHendle}
                               newMessage={newMessage}
                               sendMessage={sendMessage}
+                              message={message}
                          /> : 'Please Select your Friend'
                     }
 
